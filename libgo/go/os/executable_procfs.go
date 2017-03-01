c5acf0ce09e61ff623847a35a99da465b8571609
Matthieu Sarter <matthieu.sarter.external@atos.net>
2017-03-01 17:57:53 +0100

    libgo: build tags for aix
    Build tags for the libgo source files required to build
    libgo on AIX.
    
    Issue golang/go#19200
    
    Change-Id: I9b52bec358ced32a144a9b1276d7c9d5b0daa0a6
    Reviewed-on: https://go-review.googlesource.com/37633
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- libgo/go/os/executable_procfs.go
+++ libgo/go/os/executable_procfs.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build linux netbsd openbsd dragonfly nacl
+// +build aix linux netbsd openbsd dragonfly nacl
 
 package os
 
