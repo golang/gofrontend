ff626470294237ac664127894826614edc46a3d0
Matthieu Sarter <matthieu.sarter.external@atos.net>
2017-03-06 17:31:21 +0100

    runtime: handle ERESTART errno with AIX's wait4
    On AIX, wait4 may return with errno set to ERESTART, which causes unexepected
    behavior (for instance, go build may exit with the message "wait: restart
    system call" after running a command, even if it was successfull).
    
    Issue golang/go#19200
    
    Change-Id: I1397ff070c2a2081b2d5820cb1e8cb85dd160a62
    Reviewed-on: https://go-review.googlesource.com/37846
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- libgo/go/syscall/libcall_wait4_aix.go
+++ libgo/go/syscall/libcall_wait4_aix.go
@@ -2,18 +2,22 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !aix
-
-// For systems with the wait4 library call.
+// Handle AIX's wait4 specific behavior
 
 package syscall
 
-//sys	wait4(pid Pid_t, status *_C_int, options int, rusage *Rusage) (wpid Pid_t, err error)
+//sys  wait4(pid Pid_t, status *_C_int, options int, rusage *Rusage) (wpid Pid_t, err error)
 //wait4(pid Pid_t, status *_C_int, options _C_int, rusage *Rusage) Pid_t
 
 func Wait4(pid int, wstatus *WaitStatus, options int, rusage *Rusage) (wpid int, err error) {
 	var status _C_int
-	r, err := wait4(Pid_t(pid), &status, options, rusage)
+	var r Pid_t
+	err = ERESTART
+	// AIX wait4 may return with ERESTART errno, while the processus is still
+	// active.
+	for err == ERESTART {
+		r, err = wait4(Pid_t(pid), &status, options, rusage)
+	}
 	wpid = int(r)
 	if wstatus != nil {
 		*wstatus = WaitStatus(status)
